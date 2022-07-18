e55ae08cde193cf762d1fa0d2de9f03b9d97b519
umairkhalid <umairkhalid@fastmail.fm>
2022-07-18 12:19:05 +1000

    typeDefs established

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,51 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    type User {
+        _id: ID
+        username: String!
+        email: String!
+        bookCount: Int
+        savedBooks: [Book]
+    }
+
+    type Book {
+        bookId: ID!
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    input BookInput {
+        bookId: ID!
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+
+    type Query {
+        users: [User]
+        user: (username: String!): User
+        books: (username: String): [Book]
+        book: (bookId: ID!): Book
+        me: User
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(input: BookInput): User
+        removeBook: (bookId: ID!): User
+    }
+`;
+
+module.exports = typeDefs;
